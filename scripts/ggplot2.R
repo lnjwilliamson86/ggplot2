dc15c5b9331dacf519d6cc0c4c69e8cccb89e986
Williamson <wil9e9@csiro.au>
2019-10-03 11:07:21 +1000

    updated file name and script

--- scripts/ggplot2.R
+++ scripts/ggplot2.R
@@ -1,5 +1,5 @@
 library(tidyverse)
 
-gapminder<-read_csv("data/gapminder.csv")# read gapminder csv and assign to gapminder variable
+gapminder<-read_csv("data/gapminder_data.csv")# read gapminder csv and assign to gapminder variable
 
 gapminder_1977<-filter(gapminder,year==1977) #filter data for 1977 only
